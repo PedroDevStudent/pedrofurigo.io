21410c2ff8c123436e72fd2da3ba08ef2b9587a0
PedroDevStudent <110186700+PedroDevStudent@users.noreply.github.com>
2022-09-26 10:03:29 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,50 @@
-// Contact
-const name = document.getElementById('name');
+class MobileNavbar {
+    constructor(mobileMenu, navList, navLinks) {
+      this.mobileMenu = document.querySelector(mobileMenu);
+      this.navList = document.querySelector(navList);
+      this.navLinks = document.querySelectorAll(navLinks);
+      this.activeClass = "active";
+  
+      this.handleClick = this.handleClick.bind(this);
+    }
+  
+    animateLinks() {
+      this.navLinks.forEach((link, index) => {
+        link.style.animation
+          ? (link.style.animation = "")
+          : (link.style.animation = `navLinkFade 0.5s ease forwards ${
+              index / 7 + 0.3
+            }s`);
+      });
+    }
+  
+    handleClick() {
+      this.navList.classList.toggle(this.activeClass);
+      this.mobileMenu.classList.toggle(this.activeClass);
+      this.animateLinks();
+    }
+  
+    addClickEvent() {
+      this.mobileMenu.addEventListener("click", this.handleClick);
+    }
+  
+    init() {
+      if (this.mobileMenu) {
+        this.addClickEvent();
+      }
+      return this;
+    }
+  }
+  
+  const mobileNavbar = new MobileNavbar(
+    ".mobile-menu",
+    ".nav-list",
+    ".nav-list li",
+  );
+  mobileNavbar.init();
+
+  // Contact
+const nname = document.getElementById('name');
 const email = document.getElementById('email');
 const message = document.getElementById('message');
 const contactForm = document.getElementById('Contact-form');
@@ -7,29 +52,25 @@ const errorElement = document.getElementById('error');
 const successMsg = document.getElementById('success-msg');
 const submitBtn = document.getElementById('submit');
 
-const validate = (e) => {
+const validate = (e) => { //validate all informations, and error messages
     e.preventDefault();
-
-    if (name.value.lenght <2) {
+    if (nname.value.length <2) {
         errorElement.innerHTML = 'Your Name or company name should be at least 2 characters long.';
         return false;
-    }
-    if (!(email.value.includes('.') && (email.value.includes('@')))) {
+    } if (!(email.value.includes('.') && (email.value.includes('@')))) {
         errorElement.innerHTML = 'Please enter a valid email address.';
         return false;
-    }
-    if (!emailIsValid(email.value)) {
+    } if (!emailIsValid(email.value)) {
         errorElement.innerHTML = 'Please enter a valid email address.';
     }
     errorElement.innerHTML = '';
-    successMsg.innerHTML = 'Thank you! I will get back to you as soon as possible';
+    successMsg.innerHTML =('Thank you! I will get back to you as soon as possible');
 
     e.preventDefault();
     setTimeout(function () {
         successMsg.innerHTML = '';
         document.getElementById('contact-form').reset();
     }, 6000);
-
     return true;
 }
 
@@ -38,5 +79,3 @@ const emailIsValid = email => { //validate email, for fake mails, or spam mails
 }
 
 submitBtn.addEventListener('click', validate);
-
-//responsive topnav
